63478fe6505bc140fda2378bf83e11a8d7a189bc
non <n@n.n>
2014-06-06 01:11:17 -0700

    Tap on players

--- Assets/PlayersControls.js
+++ Assets/PlayersControls.js
@@ -7,9 +7,34 @@ var speed: float = 10;
 
 
 function Start () {
-
 }
 
+function Update() {
+    if (Input.GetMouseButtonDown(0)) {
+    	//Debug.Log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+    	//Debug.Log("pos: " + Input.mousePosition);
+    	var ray : Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    	var hit : RaycastHit2D = Physics2D.Raycast(ray.origin, ray.direction);
+
+		if (hit) {
+			if (hit.collider != null) {
+				Debug.Log(hit.collider.name);
+			} else {
+				Debug.Log("is null");
+			}
+		} else {
+			Debug.Log("asassa");
+		}
+		
+        //var wp : Vector3 = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+        //var touchPos : Vector2 = new Vector2(wp.x, wp.y);
+    	//if (collider2D == Physics2D.OverlapPoint(touchPos)) {
+        ///	Debug.Log("touch !!!!!!!!!!");
+    	//}
+	}
+}
+
+/*
 function Update ()
 {
     if(Input.GetKey(moveUp))
@@ -26,3 +51,4 @@ function Update ()
     }
     rigidbody2D.velocity.x = 0;
 }
+*/

4d5dc0b628e4c13189e1585618d8db2579d119df
kma <kma@innerjob.com>
2014-06-16 15:07:58 -0700

    Multi object drag & drop

--- Assets/PlayersControls.js
+++ Assets/PlayersControls.js
@@ -6,6 +6,9 @@ var moveDown: KeyCode;
 var speed: float = 10;
 
 var playerTouches: int[] = [-1, -1];
+var playerTransforms: Transform[] = new Transform[2];
+var playerDistances: float[] = [0.0, 0.0];
+var playerOffsets: Vector3[] = new Vector3[2];
 
 var dist: float;
 var dragging: boolean = false;
@@ -15,6 +18,15 @@ var toDrag: Transform;
 
 function Start () {
     playerTouches = [-1, -1];
+    playerDistances = [0.0, 0.0];
+
+    playerTransforms = new Transform[2];
+    playerTransforms[0] = null;
+    playerTransforms[1] = null;
+
+    playerOffsets = new Vector3[2];
+    //playerOffsets[0] = null;
+    //playerOffsets[1] = null;
 }
 
 function resetPlayer(touchNumber: int) {
@@ -32,45 +44,54 @@ function getCollider(vec: Vector2) {
     if (hit) {
         if (hit.collider != null) {
             Debug.Log(hit.collider.name);
+            var index: int = -1;
+            if(hit.collider.name == "Player01") {
+                index = 0;
+            } else if(hit.collider.name == "Player02") {
+                index = 1;
+            }
 
-            toDrag = hit.transform;
-            dist = hit.transform.position.z - Camera.main.transform.position.z;
-            var v3: Vector3 = new Vector3(vec.x, vec.y, dist);
-            v3 = Camera.main.ScreenToWorldPoint(v3);
-            offset = toDrag.position - v3;
-            dragging = true;
+            if(index >= 0) {
+                Debug.Log("index: " + index);
+                playerTransforms[index] = hit.transform;
+                playerDistances[index] = hit.transform.position.z - Camera.main.transform.position.z;
+                var v3: Vector3 = new Vector3(vec.x, vec.y, playerDistances[index]);
+                v3 = Camera.main.ScreenToWorldPoint(v3);
+                playerOffsets[index] = playerTransforms[index].position - v3;
+            }
 
-
-            return hit.collider.name;
+            return hit;
         } else {
-            Debug.Log("is null");
-            return "null";
+            return;
         }
     } else {
-        Debug.Log("empty");
-        return "";
+        return;
     }
-    return "";
+    return;
 }
 
 function processTouch(touch: Touch, touchNumber: int) {
 
     if(touch.phase == TouchPhase.Began) {
-        var colliderName: String = getCollider(touch.position);
-        if(colliderName == "Player01" && playerTouches[0] == -1) {
-            playerTouches[0] = touchNumber;
-        } else if(colliderName == "Player02" && playerTouches[1] == -1) {
-            playerTouches[1] = touchNumber;
+        var colliderHit: RaycastHit2D;
+        colliderHit = getCollider(touch.position);
+        if(colliderHit) {
+            if(colliderHit.collider.name == "Player01" && playerTouches[0] == -1) {
+                playerTouches[0] = touchNumber;
+            } else if(colliderHit.collider.name == "Player02" && playerTouches[1] == -1) {
+                playerTouches[1] = touchNumber;
+            }
         }
     } else if(touch.phase == TouchPhase.Moved) {
-        Debug.Log("touchNumber : " + touchNumber);
-        Debug.Log("playerTouches : " + playerTouches[0]);
-        if(touchNumber == playerTouches[0]) {
-            Debug.Log("touchNumber == playerTouches[0]");
-
-            var v3: Vector3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
-            v3 = Camera.main.ScreenToWorldPoint(v3);
-            toDrag.position = v3 + offset;
+        Debug.Log("Move : " + touchNumber);
+        Debug.Log("Touches : " + playerTouches[0] + "   " + playerTouches[1]);
+        for(var i = 0; i < playerTouches.Length; ++i) {
+            if(touchNumber == playerTouches[i]) {
+                //var v3: Vector3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, playerDistances[i]);
+                var v3: Vector3 = new Vector3(touch.position.x, touch.position.y, playerDistances[i]);
+                v3 = Camera.main.ScreenToWorldPoint(v3);
+                playerTransforms[i].position = v3 + playerOffsets[i];
+            }
         }
     } else if(touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled) {
         resetPlayer(touchNumber);
@@ -89,21 +110,3 @@ function Update() {
     }
 }
 
-/*
-function Update ()
-{
-    if(Input.GetKey(moveUp))
-    {
-        rigidbody2D.velocity.y = speed;
-    }
-    else if(Input.GetKey(moveDown))
-    {
-        rigidbody2D.velocity.y = speed * (-1);
-    }
-    else
-    {
-        rigidbody2D.velocity.y = 0;
-    }
-    rigidbody2D.velocity.x = 0;
-}
-*/

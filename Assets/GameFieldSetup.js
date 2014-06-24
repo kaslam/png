eb5ded8d78d5e604f5f195997c81129130f1586f
kma <kma@innerjob.com>
2014-06-24 15:07:42 -0700

    Some improvenment

--- Assets/GameFieldSetup.js
+++ Assets/GameFieldSetup.js
@@ -14,6 +14,9 @@ var yellow_v: Transform;
 
 
 function Start () {
-    blue_v.position = new Vector2(0f, mainCam.ScreenToWorldPoint (new Vector3 (0f, 0f, 0f)).y - 0.5f);
+    blue_v.position = new Vector2(-4.7f, mainCam.ScreenToWorldPoint (new Vector3 (0f, 0f, 0f)).y);
+    blue_v.transform.localScale.y = Screen.width / 500.0;
+    green_v.position = new Vector2(4.7f, mainCam.ScreenToWorldPoint (new Vector3 (0f, 0f, 0f)).y);
+    green_v.transform.localScale.y = Screen.width / 500.0;
 }
 
